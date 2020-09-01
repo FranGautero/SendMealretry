c5f82ead447171162ab5a6972f01b653d2282e2d
Francisco Gautero <francisco.gautero@gmail.com>
2020-09-01 16:39:59 -0300

    Lab 01 commit 2 Layout creado

--- app/src/main/java/gautero/tuma/sendmealretry/MainActivity.java
+++ app/src/main/java/gautero/tuma/sendmealretry/MainActivity.java
@@ -3,6 +3,17 @@ package gautero.tuma.sendmealretry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nombre = findViewById(R.id.editTextTextPersonName);
+        EditText pass = findViewById(R.id.editTextTextPassword);
+        EditText passValidation = findViewById(R.id.editTextTextPassword2);
+        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        RadioButton debito = findViewById(R.id.radioButton);
+        RadioButton credito = findViewById(R.id.radioButton2);
+        EditText numeroTarjeta = findViewById(R.id.editTextNumber);
+        EditText cvv = findViewById(R.id.editTextNumber2);
+        EditText mesVencimiento = findViewById(R.id.editTextNumber3);
+        EditText yearVencimiento = findViewById(R.id.editTextNumber4);
+        EditText cbu = findViewById(R.id.editTextNumber5);
+        EditText aliasCbu = findViewById(R.id.editTextTextPersonName2);
+        Switch cargaInicial = findViewById(R.id.switch1);
+        SeekBar sb1 = findViewById(R.id.seekBar2);
+        CheckBox tyc = findViewById(R.id.checkBox);
+        Button Registrar = findViewById(R.id.button);
+
+
+
+
+
     }
 }
