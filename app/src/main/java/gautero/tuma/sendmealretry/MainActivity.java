7913e4957479fbd061e06422217396f885c585e7
Francisco Gautero <francisco.gautero@gmail.com>
2020-09-02 18:25:31 -0300

    Lab 01 commit 4

--- app/src/main/java/gautero/tuma/sendmealretry/MainActivity.java
+++ app/src/main/java/gautero/tuma/sendmealretry/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Boolean ValidationsOK;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         EditText nombre = findViewById(R.id.editTextTextPersonName);
         EditText pass = findViewById(R.id.editTextTextPassword);
         EditText passValidation = findViewById(R.id.editTextTextPassword2);
-        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        final EditText email = findViewById(R.id.editTextTextEmailAddress);
         RadioButton debito = findViewById(R.id.radioButton);
         RadioButton credito = findViewById(R.id.radioButton2);
         EditText numeroTarjeta = findViewById(R.id.editTextNumber);
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         EditText aliasCbu = findViewById(R.id.editTextTextPersonName2);
         Switch cargaInicial = findViewById(R.id.switch1);
         CheckBox tyc = findViewById(R.id.checkBox);
+        Button Registrar = findViewById(R.id.button);
 
 
         //Cuando el Switch de carga inicial esta en true la seeker bar se activa
@@ -48,12 +50,11 @@ public class MainActivity extends AppCompatActivity {
                 Switch cargaInicial = findViewById(R.id.switch1);
                 SeekBar sb1 = findViewById(R.id.seekBar2);
                 TextView tv1 = findViewById(R.id.textView4);
-                if (cargaInicial.isChecked()){
+                if (cargaInicial.isChecked()) {
 
                     tv1.setVisibility(View.VISIBLE);
                     sb1.setVisibility(View.VISIBLE);
-                }
-                else {
+                } else {
                     tv1.setVisibility(View.GONE);
                     sb1.setVisibility(View.GONE);
                 }
@@ -79,11 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 EditText cvv = findViewById(R.id.editTextNumber2);
                 EditText mesVencimiento = findViewById(R.id.editTextNumber3);
                 EditText yearVencimiento = findViewById(R.id.editTextNumber4);
-                if(editable.length()==20){
+                if (editable.length() == 20) {
                     cvv.setEnabled(true);
                     mesVencimiento.setEnabled(true);
                     yearVencimiento.setEnabled(true);
-                }else{
+                } else {
                     cvv.setEnabled(false);
                     mesVencimiento.setEnabled(false);
                     yearVencimiento.setEnabled(false);
@@ -119,31 +120,112 @@ public class MainActivity extends AppCompatActivity {
                 //consultar como es el tema de la variable v
                 CheckBox tycAux = findViewById(R.id.checkBox);
                 if (tycAux.isChecked()) {
-                    Button Registrar = findViewById(R.id.button);
-                    Registrar.setEnabled(true);
-                }else{
-                    Button Registrar = findViewById(R.id.button);
-                    Registrar.setEnabled(false);
+                    Button RegistrarAux = findViewById(R.id.button);
+                    RegistrarAux.setEnabled(true);
+                } else {
+                    Button RegistrarAux = findViewById(R.id.button);
+                    RegistrarAux.setEnabled(false);
                 }
             }
 
         });
 
+        Registrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Boolean registroOk = true;
+
+                // 1.. valida email
+                EditText emailAux = findViewById(R.id.editTextTextEmailAddress);
+                if (!emailAux.getText().toString().isEmpty()) {
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailAux.getText().toString()).matches()) {
+                        Toast.makeText(MainActivity.this, "Email Inválido", Toast.LENGTH_SHORT).show();
+                        registroOk = false;
+                    }else registroOk = true;
+                }else {
+                    Toast.makeText(MainActivity.this, "Falta ingresar Email", Toast.LENGTH_SHORT).show();
+                    registroOk = false;
+                }
+
+                // 2.. valida clave
+
+                EditText passAux = findViewById(R.id.editTextTextPassword);
+                EditText passValidationAux = findViewById(R.id.editTextTextPassword2);
+
+                if(!passAux.getText().toString().isEmpty()){
+
+                    if(passAux.getText().toString().equals(passValidationAux.getText().toString())){
+                        registroOk = true;
+                    }else{
+                        Toast.makeText(MainActivity.this, "Las Claves no Coinciden", Toast.LENGTH_SHORT).show();
+                        registroOk = false;
+                    }
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Falta ingresar Clave", Toast.LENGTH_SHORT).show();
+                    registroOk = false;
+                }
+
+                // 3.. valida Tarjeta
+
+                EditText numeroTarjetaAux = findViewById(R.id.editTextNumber);
+                if(!numeroTarjetaAux.getText().toString().isEmpty()){
+                    if(numeroTarjetaAux.getText().toString().length() == 20){
+                        registroOk = true;
+                    }else{
+                        Toast.makeText(MainActivity.this, "Nro de Tarjeta Invalido", Toast.LENGTH_SHORT).show();
+                        registroOk = false;
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Falta Número de Tarjeta", Toast.LENGTH_SHORT).show();
+                    registroOk = false;
+                }
+
+
+                if(registroOk) Toast.makeText(MainActivity.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+
         //Validaciones
 
-        boolean emailValido;
-        if(email.getText().toString().length()>0){
-            if(email.getText().toString().split("@", 2).length > 0){
+        boolean emailValido = false;
+        if (!email.getText().toString().isEmpty()) {
+            if (email.getText().toString().split("@", 2).length > 0) {
                 emailValido = true;
-            }else emailValido = false;
-        }else emailValido = false;
+            }
+        }
 
-        boolean claveValida;
-        if(pass.getText().toString().length()>0 && passValidation.getText().toString().length()>0){
-            if(pass.getText().toString() == passValidation.getText().toString()){
+        boolean claveValida = false;
+        if (!pass.getText().toString().isEmpty() && !passValidation.getText().toString().isEmpty()) {
+            if (pass.getText().toString() == passValidation.getText().toString()) {
                 claveValida = true;
-            }else claveValida = false;
-        }else claveValida = false;
+            }
+        }
+
+        Boolean tipoTarjeta = false;
+        if (debito.isChecked() || credito.isChecked()) {
+            tipoTarjeta = true;
+        }
+
+        boolean cvvValido = false;
+        EditText cvvAux = findViewById(R.id.editTextNumber2);
+        if (cvvAux.getText().toString().length() > 3) cvvValido = true;
+
+        Boolean mesValido = false;
+        Boolean añoValido = false;
+
+        EditText mesVencimientoAux = findViewById(R.id.editTextNumber3);
+        EditText yearVencimientoAux = findViewById(R.id.editTextNumber4);
+
+        if (!mesVencimientoAux.getText().toString().isEmpty() && !yearVencimientoAux.getText().toString().isEmpty()){
+
+        }
+
 
 
     }
