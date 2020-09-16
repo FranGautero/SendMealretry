aa391158ba6abe81ad24e9eda86955aef26f5451
Francisco Gautero <francisco.gautero@gmail.com>
2020-09-16 12:56:20 -0300

    Lab 02 commit 3

--- app/src/main/java/gautero/tuma/sendmealretry/HomeActivity.java
+++ app/src/main/java/gautero/tuma/sendmealretry/HomeActivity.java
@@ -5,42 +5,45 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
-    Button irAct1, irAct2, irAct3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        irAct1 = findViewById(R.id.buttonTOA1);
-        irAct1.setOnClickListener(new View.OnClickListener() {
+        Toolbar tb = findViewById(R.id.toolbarHome);
+
+        tb.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(i);
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings1:
+                        Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                        startActivity(i);
+                        return false;
+
+                    case R.id.action_settings2:
+                        Intent i2 = new Intent(HomeActivity.this, AddPlato.class);
+                        startActivity(i2);
+                        return false;
+
+                    case R.id.action_settings3:
+                        Intent i3 = new Intent(HomeActivity.this, SelectPlato.class);
+                        startActivity(i3);
+                        return false;
+                    default:
+                        return false;
+
+                }
             }
         });
-        irAct2 = findViewById(R.id.buttonTOA2);
-        irAct2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, AddPlato.class);
-                startActivity(i);
-            }
-        });
-        irAct3 = findViewById(R.id.buttonTOA3);
-        irAct3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, SelectPlato.class);
-                startActivity(i);
-            }
-        });
+
 
     }
 }
